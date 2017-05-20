3286acd86b78773d526cd6176cc6102eab2bbf9c
rbante <rbante@gmail.com>
2017-05-20 18:44:28 +0800

    page classes

---
+++ src/test/java/pageClasses/HomePage.java
@@ -0,0 +1,13 @@
+package pageClasses;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+	WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/test/java/pageClasses/MyAccountPage.java
+++ src/test/java/pageClasses/MyAccountPage.java
@@ -1,14 +1,32 @@
 package pageClasses;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MyAccountPage {
 
 	WebDriver driver;
 	
+	@FindBy(className = "page-heading")
+	public WebElement pageHeading;
+	
+	@FindBy(xpath = ".//*[@id='center_column']/ul/li/a/span")
+	public WebElement homeButton;
+	
+	
 	public MyAccountPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 		this.driver = driver;
+	}
+	
+	public void verifyAccountPage(){
+		System.out.println(pageHeading.getText());
+	}
+	
+	public HomePage goToHomePage(){
+		homeButton.click();
+		return new HomePage();
 	}
 
 }
